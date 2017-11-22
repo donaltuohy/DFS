40d712b57d49d9c07c1448b705b28c0c6a22c765
donaltuohy <tuohydo@tcd.ie>
2017-11-22 23:54:11 +0000

    Very basics

--- mainServer.py
+++ mainServer.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, url_for, request, session, redirect
-#from flask.ext.pymongo import PyMongo
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return "Hi there how you doing"
+app.config['MONGO_DBNAME'] = 'dfs'
+app.config['MONGO_URI'] = 'mongodb://donal:testing@ds117156.mlab.com:17156/dfs'
+
+mongo = PyMongo(app)
+
+@app.route('/add')
+def add():
+    files = mongo.db.files
+    files.insert({'name': 'testingFilename'})
+    return "Added name!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
