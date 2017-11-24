b0b0d065d050e8553914c7e5df67d2f7334030c1
donaltuohy <tuohydo@tcd.ie>
2017-11-24 22:01:35 +0000

    Have file uploads working through a html form

--- mainServer.py
+++ mainServer.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template, url_for, request, session, redirect
+import os
+from flask import Flask, render_template, url_for, request, session, flash, redirect, send_from_directory
 from flask.ext.pymongo import PyMongo
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER ='/home/donal-tuohy/Documents/SS_year/DFS/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-
 app.config['MONGO_DBNAME'] = 'dfs'
 app.config['MONGO_URI'] = 'mongodb://donal:testing@ds117156.mlab.com:17156/dfs'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        #Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        #if user does not select file
+        if file.filename =='':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 mongo = PyMongo(app)
 
