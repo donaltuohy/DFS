09db6c91017aa484cf62498f71317515021d569a
donaltuohy <tuohydo@tcd.ie>
2017-11-28 17:33:47 +0000

    When a file is upload, it wil be sent tp the node with the least number of files

--- mainServer.py
+++ mainServer.py
@@ -9,7 +9,7 @@ listOfFiles = {}
 fileAccessCount = {}
 
 #dict - key = nodeID, contains list of all node addresses
-#listOffiles[1] = "http://127.0.0.1:5001/"
+#connectedNodes[1] = ["http://127.0.0.1:5001/", 2(This is thenumberOfFiles)]
 connectedNodes = {}
 
 UPLOAD_FOLDER ='/home/donal-tuohy/Documents/SS_year/DFS/uploads'
@@ -19,75 +19,108 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisShouldBeSecret'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+### LOCAL FUNCTIONS ###
+
+#Pass in an address and it will return the node ID as an int
 def parseNodeID(address):
     split = address.split(':')
     port = int((split[2])[:4])
     return port - 5000
 
-
+#Pass in a dict of files {"index.jpeg": [address1, address2]}
 def addFilesFromNode(dictOfFiles, nodeAddress):
+
+    #for each file in the passed in dictionary
     for fileName in dictOfFiles:
+        #Check if there is a file with this name already on the server
         if fileName in listOfFiles.keys():
+            #If the address of the current node isn't already stored, add it to the list of addresses associated with this file
             if nodeAddress not in listOfFiles[fileName]:
                 (listOfFiles[fileName]).append(nodeAddress)
                 (fileAccessCount[fileName])[1] += 1     #Add to the total number of nodes that have the file 
-                
+        #If the file does not exist on the global list of files, create a new key with the filename        
         else:
             listOfFiles[fileName] = [nodeAddress]
-            fileAccessCount[fileName] = [0,1]       #First index is amount of times it has been accesed, second is number of nodes that have it
+            fileAccessCount[fileName] = [0,1]       #First index is amount of times it has been accessed, second is number of nodes that have it
 
+
+#This works out where the file was downloaded most recently and preforms round robin to make sure the file is downloaded fairly from nodes
 def roundRobin(filename):
     myList = fileAccessCount[filename]
     indexOfNodeToUse = (myList[0])%(myList[1])
     (fileAccessCount[filename])[0] += 1
     return indexOfNodeToUse 
-    
-#If client wants to get a file, it sends a get request to this URL
-#This server checks it's list of files and returns the address of the node which should be accessed next
-@app.route('/download/<filename>')
-def download_file(filename):
 
-    if filename in listOfFiles.keys():
-        nodeAddress = ((listOfFiles[filename])[roundRobin(filename)])
-        return jsonify({'message': 'File exists.', 'address': ( nodeAddress + filename), 'nodeID': parseNodeID(nodeAddress)})
-    else:
-        return jsonify({'message': 'File does not exist.'})
+def nodeToUploadTo():
+    minIndex = 1
+    minValue = ((connectedNodes[1])[1])
+    for node in connectedNodes.keys():
+        if(connectedNodes[node])[1] < minValue:
+            minIndex = node
+    return (connectedNodes[minIndex])[0]    #Returning address to upload to
 
+### ENDPOINTS ###
+
+
+#When a new node is created it will send a POST request to this endpoint
+#The request will contain a json file with all the node's details and files that it has
 @app.route('/newnode', methods=['POST'])
 def newNode():
     response = request.get_json()
-    newNodeID = response['nodeID']
-    newNodeAddr = response['address']
-    dictOfFiles = response['currentFiles']
-    addFilesFromNode(dictOfFiles, newNodeAddr)
-    print("Global list of files: ",listOfFiles)
-    connectedNodes[newNodeID] = newNodeAddr
+    if (response):
+        newNodeID = response['nodeID']
+        newNodeAddr = response['address']
+        dictOfFiles = response['currentFiles']
+        addFilesFromNode(dictOfFiles, newNodeAddr)
+        print("Global list of files: ",listOfFiles)
+        connectedNodes[newNodeID] = [newNodeAddr, len(dictOfFiles)]
+        return jsonify({'message': 'Node successfuly set up.'})
+    else:
+        return jsonify({'message': 'Node could not be set up.'})
 
-    print("Node " + str(newNodeID) +" connected on " + newNodeAddr + ".")
-    print(connectedNodes)
-    return "Hello" #request.get_json()
-
-@app.route('/uploadcheck/<filename>', methods=['GET'])
-def upload_file_check(filename):
-    print("Filename: ", filename)
-    if filename in listOfFiles:
-        return jsonify({'message': 'File already exists.', 'nodeAddresses': listOfFiles[filename]})
-    return jsonify({'message': 'File does not exist.'})
-
+#This endpoint is accessed by a node when it gets a new file. It updates the list of files
 @app.route('/newfile', methods=['POST'])
 def newFile():
     data = request.get_json()
     nodeAddress = data['nodeAddress']
     dictOfFile = data['fileName']
     addFilesFromNode(dictOfFile,nodeAddress)
+    (connectedNodes[parseNodeID(nodeAddress)])[1] += 1
+    print(connectedNodes)
     return "File added to global list"
 
+#This endpoint returns a dict of all the files stored on the server
+@app.route('/returnlist', methods=['GET'])
+def returnFiles():
+    return jsonify(listOfFiles)
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+#This endpoint is used to check if a file exists on the server.
+@app.route('/uploadcheck/<filename>', methods=['GET'])
+def upload_file_check(filename):
+    print("Filename: ", filename)
+    if filename in listOfFiles:
+        return jsonify({'message': 'File already exists.', 'nodeAddresses': listOfFiles[filename]})
+    
+    return jsonify({'message': 'File does not exist.', "addressToUploadTo": nodeToUploadTo() })
 
+
+
+
+
+#@app.route('/uploads/<filename>')
+#def uploaded_file(filename):
+#    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+#If client wants to get a file, it sends a get request to this URL
+#This server checks it's list of files and returns the address of the node which should be accessed next
+@app.route('/download/<filename>')
+def download_file(filename):
+    if filename in listOfFiles.keys():
+        nodeAddress = ((listOfFiles[filename])[roundRobin(filename)])
+        return jsonify({'message': 'File exists.', 'address': ( nodeAddress + filename), 'nodeID': parseNodeID(nodeAddress)})
+    else:
+        return jsonify({'message': 'File does not exist.'})
 
 
 if __name__ == "__main__":
